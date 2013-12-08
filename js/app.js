95b7375c64172301dd956256e8a34f13a966d043
Esteban R <estebarb@gmail.com>
2013-12-08 13:54:43 -0600

    Se completó la GUI para agregar facturas.

--- js/app.js
+++ js/app.js
@@ -3,14 +3,55 @@ var facturasApp = angular.module('facturasApp', []);
 
 //Controlador
 facturasApp.controller('mainController', function($scope, $location){
-		$scope.msg = "este es un mensaje desde el scope";
+	// Esto es para el sistema de facturas nuevas
+	var now = Date();
+	$scope.NewBill = {
+		"emisor": "",
+		"cliente": "",
+		"fecha": now,
+		"current": {
+			"descripcion": "",
+			"cantidad": 0,
+			"precio": 0
+		},
+		"productos": []
+	};
 
-		$scope.isActive = function (viewLocation) {
-				var active = (viewLocation === $location.path());
-				if(viewLocation === "/" &&
-						$location.path() === ""	){
-								active = true;
-						}
-				return active;
-		};
+	sumador = function(v, acc){
+		return v + acc;
+	};
+
+	$scope.CalculaSubtotal = function(){
+		if($scope.NewBill.productos.length == 0){
+			return 0;
+		} else {
+			return $scope.NewBill.productos.map(function(x){
+				return x.cantidad * x.precio;
+			}).reduce(sumador, 0);
+		}
+	};
+
+	$scope.CalculaImpuestos = function(){
+		return $scope.CalculaSubtotal() * 0.13;
+	};
+
+	$scope.CalculaDescuento = function(){
+		return $scope.CalculaSubtotal() * 0.10;
+	};
+
+	$scope.CalculaTotal = function(){
+		return $scope.CalculaSubtotal
+		       	+ $scope.CalculaImpuestos()
+		       	- $scope.CalculaDescuento();
+	};
+
+	// Esto es necesario para poder cambiar de páginas
+	$scope.isActive = function (viewLocation) {
+		var active = (viewLocation === $location.path());
+		if(viewLocation === "/" &&
+			$location.path() === ""	){
+				active = true;
+			}
+		return active;
+	};
 });

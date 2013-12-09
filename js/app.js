0ccc2a46d92f3f50b7aa801491c35aced0de0313
Esteban R <estebarb@gmail.com>
2013-12-08 19:47:26 -0600

    Trabajos en la portada y ver facturas

--- js/app.js
+++ js/app.js
@@ -4,11 +4,20 @@ var facturasApp = angular.module('facturasApp', ['facturasServices']);
 //Controlador
 facturasApp.controller('mainController',
 		['$scope', '$location', 'Factura', function($scope, $location, Factura){
-	// Esto es para el sistema de facturas nuevas
-	var now = Date();
-	$scope.ResetNewBill = function(){
-		$scope.NewBill = {
-			"emisor": "",
+			sumador = function(v, acc){
+				return v + acc;
+			};
+sumaCampo = function(arreglo, campo){
+	return arreglo.map(function(v){
+		return v[campo];
+	}).reduce(sumador, 0);
+}
+
+			// Esto es para el sistema de facturas nuevas
+			var now = Date();
+			$scope.ResetNewBill = function(){
+				$scope.NewBill = {
+					"emisor": "",
 	"cliente": "",
 	"fecha": now,
 	"current": {
@@ -17,68 +26,98 @@ facturasApp.controller('mainController',
 	"precio": 0
 	},
 	"productos": []
-		};
-	};
-	$scope.ResetNewBill();
+				};
+			};
+			$scope.ResetNewBill();
 
-	$scope.ListaFacturas = [];
-	$scope.LoadFacturas = function(){
-//		Factura.
-	};
-	
+			$scope.ListaFacturas = [];
+			$scope.LoadFacturas = function(){
+				Factura.query({}, function(x){
+					$scope.ListaFacturas = [];
+					var lst = [];
+					// Actualiza los valores de las facturas
+					for(var i = 0; i < x.length; i++){
+						if(x[i].productos != undefined){
+						x[i].TotalFactura = x[i].productos.map(
+							function(e){
+								return e.precio * e.cantidad;
+							}).reduce(sumador, 0);
+						} else {
+							x[i].TotalFactura = 0;
+						}
 
-	sumador = function(v, acc){
-		return v + acc;
-	};
+						if(x[i].pagos != undefined){
+						x[i].TotalPagado = x[i].pagos.map(
+							function(e){
+								return e.pago;
+							}).reduce(sumador, 0);
+						} else {
+							x[i].TotalPagado = 0;
+						}
 
-	$scope.CalculaSubtotal = function(){
-		var p = $scope.NewBill.productos;
-		if(p.length == 0){
-			return 0;
-		} else {
-			return p.map(function(x){
-				return x.cantidad * x.precio;
-			}).reduce(sumador, 0);
-		}
-	};
+					}
+					$scope.ListaFacturas = x;
+					// Actualiza valores generales del sistema:
+					$scope.general = {
+						"TotalFacturas": x.length,
+						"TotalPagado": sumaCampo(x, "TotalPagado"),
+						"TotalFacturado": sumaCampo(x, "TotalFactura"),
+						"TotalAdeudado": sumaCampo(x, "TotalFactura") - sumaCampo(x, "TotalPagado")
+					}
+				});
+			};
+			$scope.LoadFacturas();
 
-	$scope.CalculaImpuestos = function(){
-		return $scope.CalculaSubtotal() * 0.13;
-	};
 
-	$scope.CalculaDescuento = function(){
-		return $scope.CalculaSubtotal() * 0.10;
-	};
 
-	$scope.CalculaTotal = function(){
-		return $scope.CalculaSubtotal
-			+ $scope.CalculaImpuestos()
-			- $scope.CalculaDescuento();
-	};
-
-	$scope.AddToCart = function(){
-		$scope.NewBill.productos.push($scope.NewBill.current);
-		$scope.NewBill.current = {
-			"descripcion": "",
-			"cantidad": 0,
-			"precio": 0
-		};
-	}
-
-	// Esto es necesario para poder cambiar de páginas
-	$scope.isActive = function (viewLocation) {
-		var active = (viewLocation === $location.path());
-		if(viewLocation === "/" &&
-				$location.path() === ""	){
-					active = true;
+			$scope.CalculaSubtotal = function(){
+				var p = $scope.NewBill.productos;
+				if(p.length == 0){
+					return 0;
+				} else {
+					return p.map(function(x){
+						return x.cantidad * x.precio;
+					}).reduce(sumador, 0);
 				}
-		return active;
-	};
+			};
 
-	Factura.get({"id": "52a4f6f98e50816c298b4567"}, function(x){
-		console.log(x);
-	});
-	Factura.query({}, function(x){
-		console.log(x);
-	});
-}]);
+			$scope.CalculaImpuestos = function(){
+				return $scope.CalculaSubtotal() * 0.13;
+			};
+
+			$scope.CalculaDescuento = function(){
+				return $scope.CalculaSubtotal() * 0.10;
+			};
+
+			$scope.CalculaTotal = function(){
+				return $scope.CalculaSubtotal
+					+ $scope.CalculaImpuestos()
+					- $scope.CalculaDescuento();
+			};
+
+			$scope.AddToCart = function(){
+				$scope.NewBill.productos.push($scope.NewBill.current);
+				$scope.NewBill.current = {
+					"descripcion": "",
+					"cantidad": 0,
+					"precio": 0
+				};
+			}
+
+			// Esto es necesario para poder cambiar de páginas
+			$scope.isActive = function (viewLocation) {
+				var active = (viewLocation === $location.path());
+				if(viewLocation === "/" &&
+						$location.path() === ""	){
+							active = true;
+						}
+				return active;
+			};
+
+			Factura.get({"id": "52a4f6f98e50816c298b4567"}, function(x){
+				console.log(x);
+			});
+			Factura.query({}, function(x){
+				console.log(x);
+			});
+		}]);
